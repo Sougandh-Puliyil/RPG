2319937d8754615749efa204d84e7f6746859b19
Sougandh <sougandhpuliyil@gmail.com>
2026-04-24 14:39:59 +0530

    added player states and statemachine

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,23 +2,24 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction :Vector2=Vector2.DOWN
 var direction:Vector2=Vector2.ZERO
-var move_speed : float=100.0
-var state : String="idle"
+
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
 
 
 
 func _ready():
-	pass
+	state_machine.Initialize(self)
+
+
+
 func _process(delta):
 	
 	direction.x=Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y=Input.get_action_strength("down") - Input.get_action_strength("up")
-	velocity=direction*move_speed
-	if SetState() == true || SetDirection()==true:
-		UpdateAnimation()
+	
 	
 	
 func _physics_process(delta):
@@ -43,15 +44,10 @@ func SetDirection() -> bool:
 	return true
 
 
-func SetState() -> bool:
-	var new_state :String="idle" if direction == Vector2.ZERO else "walk"
-	if new_state==state:
-		return false
-	state=new_state
-	return true
 
 
-func UpdateAnimation()->void:
+
+func UpdateAnimation(state:String)->void:
 	animation_player.play(state + "_" + AnimDirection())
 	pass
 func AnimDirection()->String:
---
+++ Scripts/state_idle.gd
@@ -0,0 +1,30 @@
+class_name State_Idle extends State
+
+@onready var walk: State = $"../Walk"
+
+
+
+#What happens when the player enters this state
+func Enter() -> void:
+	player.UpdateAnimation("idle")
+	
+#what happens when player exits this state
+func Exit()-> void:
+	pass
+
+#what happens during the _process update in this state
+func Process(_delta:float)->State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity=Vector2.ZERO
+	return null
+
+
+#what happens during the _physics_process update inthis state
+func Physics(_delta:float)->State:
+	return null
+
+
+#what happens with input events in this state
+func HandleInput( _event:InputEvent)->State:
+	return null
