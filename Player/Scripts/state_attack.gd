d53f124a0ec926ca88c253886d90187422839b80
Sougandh <sougandhpuliyil@gmail.com>
2026-04-30 18:25:50 +0530

    E05 complete

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -1,17 +1,25 @@
 class_name State_Attack extends State
 
 var attacking :bool =false
-@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+@export var attack_sound: AudioStream
+@export_range(1,20,0.5) var decelerate_speed:float=5.0
 
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var walk: State = $"../Walk"
 @onready var idle: State = $"../Idle"
+@onready var attack_anim: AnimationPlayer = $"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
+@onready var audio: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
 
 
 
 #What happens when the player enters this state
 func Enter() -> void:
 	player.UpdateAnimation("attack")
+	attack_anim.play("attack_"+player.AnimDirection())
 	animation_player.animation_finished.connect( EndAttack)
+	audio.stream=attack_sound
+	audio.pitch_scale=randf_range(0.9,1.1)
+	audio.play()
 	attacking=true
 	
 #what happens when player exits this state
@@ -22,7 +30,7 @@ func Exit()-> void:
 
 #what happens during the _process update in this state
 func Process(_delta:float)->State:
-	player.velocity=Vector2.ZERO
+	player.velocity-=player.velocity * decelerate_speed * _delta
 	if attacking==false:
 		if player.direction==Vector2.ZERO:
 			return idle
